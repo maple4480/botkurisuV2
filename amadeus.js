1927cd6e7ce9c4186194afa313dc5049b863cb34
maple4480 <justinmhoang4480@yahoo.com>
2020-11-15 18:23:37 -0800

    Removed server code changes

--- amadeus.js
+++ amadeus.js
@@ -32,29 +32,12 @@ var eventHandler = new events.EventEmitter();
 //     degen,
 //     steinGate,
 // } = require('./playlist.json');
-var pathname ="WRONG";
-var http = require('http');
-var url = require('url') ;
-var server = http.createServer(function (req, res) {   
-    var hostname = req.headers.host; // hostname = 'localhost:8080'
-    pathname = url.parse(req.url).pathname; // pathname = '/MyApp'
-    console.log('http://' + hostname + pathname);
 
-    if (req.url == '/data') { //check the URL of the current request
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.write(JSON.stringify({ message: "Hello World"}));  
-            res.end();  
-    }
-});
 /*************************************************************************************************************************************/
 //When application starts do this:
 client.on('ready', () => {
     log('Bot is ready...Awaiting Input!');
-    log(pathname);
     client.user.setActivity(". For help: `help"); 
-
-    server.listen(5000);
-    console.log('Node.js web server at port 5000 is running..')
 });
 
 /*************************************************************************************************************************************/
